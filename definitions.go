0d29b3707459de78035e51fd943ec19aecd8efcb
Robert Bisewski <contact@ibiscybernetics.com>
2018-01-31 10:53:06 -0600

    adding new definitions

--- definitions.go
+++ definitions.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"archive/zip"
+)
+
 // Config holds user-provided and other settings
 type Config struct {
 
@@ -11,4 +15,24 @@ type Config struct {
 
 	// location to write the converted output
 	outputDir string
+}
+
+// Odt ... Structure for handling ODT files
+type Odt struct {
+	files    []*zip.File
+	content  string
+	settings string
+	styles   string
+	headers  map[string]string
+	footers  map[string]string
+}
+
+// ReplaceOdt ... structure for handling ODT files content replacement
+type ReplaceOdt struct {
+	zipReader *zip.ReadCloser
+	content   string
+	settings  string
+	styles    string
+	headers   map[string]string
+	footers   map[string]string
 }
