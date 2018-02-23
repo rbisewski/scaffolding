48279b496727829f03342104491f6cd395ed7fb3
Robert Bisewski <contact@ibiscybernetics.com>
2018-02-23 12:17:58 -0600

    dumping tags into easy-to-maintain globals

---
+++ globals.go
@@ -0,0 +1,19 @@
+package main
+
+const (
+	// scaffolding language tags
+	SCAFFOLDING_TABLE_TITLE_START        = ":scaffolding-table-title-start:"
+	SCAFFOLDING_TABLE_TITLE_END          = ":scaffolding-table-title-end:"
+	SCAFFOLDING_TABLE_START              = ":scaffolding-table-start-"
+	SCAFFOLDING_TABLE_COLS               = ":scaffolding-table-cols-"
+	SCAFFOLDING_TABLE_END                = ":scaffolding-table-end:"
+	SCAFFOLDING_COLUMN_TABLE             = ":scaffolding-column-table-"
+	SCAFFOLDING_ROW_START                = ":scaffolding-row-start:"
+	SCAFFOLDING_ROW_END                  = ":scaffolding-row-end:"
+	SCAFFOLDING_CELL_CENTRED_START_TABLE = ":scaffolding-cell-centred-start-table-"
+	SCAFFOLDING_CELL_START_TABLE         = ":scaffolding-cell-start-table-"
+	SCAFFOLDING_CELL_BOLD                = ":scaffolding-cell-bold:"
+	SCAFFOLDING_COVERED_CELL_END         = ":scaffolding-covered-cell-end:"
+	SCAFFOLDING_CELL_END                 = ":scaffolding-cell-end:"
+	SCAFFOLDING_ODT_SPACE                = ":scaffolding-odt-space:"
+)
